f67a0d1963ce10cc889f3d2ab8908e1b4e313e84
Carlos Eduardo Santos Freitas <carlos.freitas@valmetpartners.com>
2024-02-08 13:24:48 -0300

    Project initilized. Routas, glocal styles and theme configurated.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/themes/global"
+import { Router } from "./components/Router"
+import { BrowserRouter } from "react-router-dom"
+
+
+function App() {
+
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+    white: "#fff",
+
+    'gray-100': '#E1E1E6',
+    'gray-300': '#C4C4CC',
+    'gray-400': '#8D8D99',
+    'gray-500': '#7C7C8A',
+    'gray-600': '#323238',
+    'gray-700': '#29292E',
+    'gray-800': '#202024',
+    'gray-900': '#121214',
+
+    'red-500': '#AB222E',
+    'red-700': '#7A1921',
+
+    'yellow-500': '#FBA94C',
+
+    "purple-300": "#7A22D3",
+    "purple-500": "#5505A6",
+    "purple-700": "#3B0087",
+    
+}
